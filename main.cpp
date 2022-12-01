ad7777fc4f5a82b5f8fa27481ebfbce195d53bb8
Songha Lee <paperstory10@naver.com>
2022-12-02 04:54:49 +0900

    17829 solve

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
 	int N;
-	cout << "asd";
+	cin >> N;
+	vector <vector <int>> board(N, vector <int>(N));
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < N; ++j) {
+			cin >> board[i][j];
+		}
+	}
 	return 0;
 }
